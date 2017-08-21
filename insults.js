1ae929d157d40193783864fbef3f06bcba27be10
BrysonYuen <30877962+BrysonYuen@users.noreply.github.com>
2017-08-21 10:21:25 -1000

    Add files via upload

---
+++ insults.js
@@ -0,0 +1,4 @@
+var insults = ["You donkey", "NBA 2k17 Sucks", "Cyka Bylat", "Why the hell did you preorder Battlefront 2"];
+function Insults(){
+  window.alert(insults[Math.floor(Math.random()*4)]);
+}
