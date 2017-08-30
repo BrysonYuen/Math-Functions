6eb0e02c5957c2698bf127f63b5163dffc8534e7
BrysonYuen <30877962+BrysonYuen@users.noreply.github.com>
2017-08-30 10:14:23 -1000

    Add files via upload

---
+++ ObjectArrayPractice.js
@@ -0,0 +1,20 @@
+var brysonOne = {
+	name: "brysonYuenOne",
+  age: 16,
+  grade: 12,
+  friends: ['Bryson', 'Dyson', 'Brydaddy'],
+};
+var brysonTwo = {
+	name: "brysonYuenTwo",
+  age: 17,
+  grade: 13,
+  friends: ['Justin', 'Matthew', 'Brandon'],
+};
+var brysonThree = {
+	name: "brysonYuenThree",
+  age: 18,
+  grade: 14,
+  friends: ['Me', 'Myself', 'I'],
+};
+var x = [brysonOne, brysonTwo, brysonThree];
+window.alert(x[1].friends[0]);
