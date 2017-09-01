dd0ddda40787edc1dc183af74957c79fdf72b152
BrysonYuen <30877962+BrysonYuen@users.noreply.github.com>
2017-09-01 10:31:42 -1000

    Add files via upload

---
+++ guessingAgainstAComputer.js
@@ -0,0 +1,14 @@
+var playerOne = Math.floor((Math.random() * 6) + 1);
+if ((playerOne < 1) || (playerOne > 6) || isNaN(playerOne)) {
+	window.alert("You cannot follow directions");
+}
+else {
+	var playerTwo = prompt("Guess a number between 1-6");
+  if (playerOne == playerTwo) {
+  	window.alert("You Win!");
+  }
+  else {
+  	window.alert("Player one answer was " + playerOne);
+    window.alert("You chose " + playerTwo);
+  }
+}
